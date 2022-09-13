6b04d605ff64dba4d4409a38a898ced39b7f54c9
Zokk-Tv <113283396+Zokk-Tv@users.noreply.github.com>
2022-09-13 22:42:00 +0200

    Innit commit

---
+++ Commandes/ping.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+    name: "ping",
+    description: "Affiche la latence",
+    permission: "Aucune",
+    dm: true,
+    category: "📚 Informations",
+    
+
+    async run(bot, message, args) {
+
+        await message.reply(`Ping : \`${bot.ws.ping}\``)
+    }
+
+}
